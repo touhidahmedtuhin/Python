56fc98d85793f260ad9a2ddf7ce661d44c384730
Touhid Ahmed Tuhin <175407818+touhidahmedtuhin@users.noreply.github.com>
2025-09-29 08:33:58 +0600

    Day #(1 - 10) python code

---
+++ Day1-10/day2.py
@@ -0,0 +1,5 @@
+#This is a single line comment 
+print("This is a print statement")
+
+'''This is a multi line comment '''
+print("This is a print statement after the multi line comment is written")
