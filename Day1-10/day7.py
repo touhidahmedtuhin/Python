facf948ac562323cb4309298dac433690a221885
Touhid Ahmed Tuhin <175407818+touhidahmedtuhin@users.noreply.github.com>
2025-10-04 18:43:40 +0600

    Day #(1 - 10) python code

---
+++ Day1-10/day7.py
@@ -0,0 +1,12 @@
+a=5
+b=2
+print("the sum is" , a+b)
+print("the product is" , a*b)
+print ("the diff is" , a-b)
+print ("the division is" , a/b)
+print ("the remainder is" , a%b)
+print ("the power is" , a**b)
+print ("the floor division is" , a//b)
+
+a = 5
+print(type(a))
