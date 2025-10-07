62f725a6d7a106e48e96b8adf750229ab227d373
Touhid Ahmed Tuhin <175407818+touhidahmedtuhin@users.noreply.github.com>
2025-10-07 19:50:25 +0600

    Day #(11 - 20) python code

---
+++ day11-20/day11.py
@@ -0,0 +1,11 @@
+name = "Harry"
+print(name)
+print(name[0])
+anotherName = '''Hey i am a boy
+                 nice to see you'''
+print(anotherName)
+
+#looping through the string
+shortName = "karim"
+for character in shortName:
+  print(character)
