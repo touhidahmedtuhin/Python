68589b56829a6934403e954df49c2e32b1127f33
Touhid Ahmed Tuhin <175407818+touhidahmedtuhin@users.noreply.github.com>
2025-10-07 19:45:13 +0600

    Day #(1 - 10) python code

---
+++ Day1-10/day10.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number:")) 
+b = int(input("Enter second number:"))
+
+print("The sum of",a,"and",b,"is:",(a+b))
+print("The difference of",a,"and",b,"is:",(a-b))
+
+
+x = input("Enter your name :")
+print("welcome",x)
