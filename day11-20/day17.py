7a2f99a624e7e28145fba0478ad46961eb96467c
Touhid Ahmed Tuhin <175407818+touhidahmedtuhin@users.noreply.github.com>
2025-10-13 07:32:30 +0600

    Day #(11 - 20) python code

---
+++ day11-20/day17.py
@@ -0,0 +1,7 @@
+#for loops
+colors = ["red", "green", "blue", "yellow"]
+
+for index, color in enumerate(colors):
+    print(index, color)
+
+# In Python, enumerate() is a built-in function that helps you loop through a list while keeping track of the index of each item.
