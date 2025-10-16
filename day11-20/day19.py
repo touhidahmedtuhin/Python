9ca2b4d28585db2acb3fa90ce8a45e04e801f96c
Touhid Ahmed Tuhin <175407818+touhidahmedtuhin@users.noreply.github.com>
2025-10-16 07:18:33 +0600

    Day #(11 - 20) python code

---
+++ day11-20/day19.py
@@ -0,0 +1,21 @@
+# #break statement 
+# for i in range(1,12):
+#   print(i)
+#   if i==5:
+#     break
+
+#   #do while in python
+
+# count = 1
+# while True:
+#     print("count", count)
+#     count += 1
+#     if count == 8:
+#         break
+    
+
+for i in range(12):
+  if i == 6:
+    print("skip the iteration")
+    continue
+  print("5 x",i,"=",5*1)
