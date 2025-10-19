51d8deeb9eff130c4853cbd261eb2b1c09edb705
Touhid Ahmed Tuhin <175407818+touhidahmedtuhin@users.noreply.github.com>
2025-10-19 07:42:50 +0600

    Day #(11 - 20) python code

---
+++ day11-20/day20.py
@@ -0,0 +1,13 @@
+def add(a, b):
+  return a+b
+result = add(1,3)
+print("Sum of 1 and 3 is",result)
+
+def greet(name):
+  print("Hello",name)
+greet("tuhin")
+
+
+#we can also pass the function that we can code with later
+def isLesser(a,b):
+  pass
