8ffb5546c333e1172af8a4199467885aae48a89f
Touhid Ahmed Tuhin <175407818+touhidahmedtuhin@users.noreply.github.com>
2025-10-23 07:54:56 +0600

    Day #(21 - 30) python code

---
+++ day21-30/day24.py
@@ -0,0 +1,16 @@
+#tuples in python
+
+tup = (1,99,6,47,5,3,2,78,"karim",True)
+print(tup)
+
+if 47 in tup:
+  print("Yes 47 is in the tuple")
+else :
+  print("It is not")
+
+
+tup2 = tup[1:4]
+print(tup2)
+
+tup3 = tup[2:-2]
+print(tup3)
